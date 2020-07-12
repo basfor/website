5f125d493750edd920d3e04eb17b29925fe1efc9
Gonzalo Flores <gonzalo.flores@lana.xyz>
2020-07-12 17:08:43 -0400

    remove extra files and clear codice

--- js/main.js
+++ js/main.js
@@ -24,82 +24,18 @@
 	};
 
 	var fullHeight = function () {
-
 		if (!isMobile.any()) {
 			$('.js-fullheight').css('height', $(window).height());
 			$(window).resize(function () {
 				$('.js-fullheight').css('height', $(window).height());
 			});
 		}
-
 	};
-
-
-	var counter = function () {
-		$('.js-counter').countTo({
-			formatter: function (value, options) {
-				return value.toFixed(options.decimals);
-			},
-		});
-	};
-
-
-	var counterWayPoint = function () {
-		if ($('#colorlib-counter').length > 0) {
-			$('#colorlib-counter').waypoint(function (direction) {
-
-				if (direction === 'down' && !$(this.element).hasClass('animated')) {
-					setTimeout(counter, 400);
-					$(this.element).addClass('animated');
-				}
-			}, { offset: '90%' });
-		}
-	};
-
-	// Animations
-	var contentWayPoint = function () {
-		var i = 0;
-		$('.animate-box').waypoint(function (direction) {
-
-			if (direction === 'down' && !$(this.element).hasClass('animated')) {
-
-				i++;
-
-				$(this.element).addClass('item-animate');
-				setTimeout(function () {
-
-					$('body .animate-box.item-animate').each(function (k) {
-						var el = $(this);
-						setTimeout(function () {
-							var effect = el.data('animate-effect');
-							if (effect === 'fadeIn') {
-								el.addClass('fadeIn animated');
-							} else if (effect === 'fadeInLeft') {
-								el.addClass('fadeInLeft animated');
-							} else if (effect === 'fadeInRight') {
-								el.addClass('fadeInRight animated');
-							} else {
-								el.addClass('fadeInUp animated');
-							}
-
-							el.removeClass('item-animate');
-						}, k * 200, 'easeInOutExpo');
-					});
-
-				}, 100);
-
-			}
-
-		}, { offset: '85%' });
-	};
-
 
 	var burgerMenu = function () {
-
 		$('.js-colorlib-nav-toggle').on('click', function (event) {
 			event.preventDefault();
 			var $this = $(this);
-
 			if ($('body').hasClass('offcanvas')) {
 				$this.removeClass('active');
 				$('body').removeClass('offcanvas');
@@ -108,112 +44,57 @@
 				$('body').addClass('offcanvas');
 			}
 		});
-
-
-
 	};
-
-
-  
-
-
-
-
 
 	// Click outside of offcanvass
 	var mobileMenuOutsideClick = function () {
-
 		$(document).click(function (e) {
 			var container = $("#colorlib-aside, .js-colorlib-nav-toggle");
 			if (!container.is(e.target) && container.has(e.target).length === 0) {
-
 				if ($('body').hasClass('offcanvas')) {
-
 					$('body').removeClass('offcanvas');
 					$('.js-colorlib-nav-toggle').removeClass('active');
-
 				}
-
 			}
 		});
-
 		$(window).scroll(function () {
 			if ($('body').hasClass('offcanvas')) {
 
 				$('body').removeClass('offcanvas');
 				$('.js-colorlib-nav-toggle').removeClass('active');
-
 			}
 		});
-
 	};
 
 	var clickMenu = function () {
-
 		$('#navbar a:not([class="external"])').click(function (event) {
 			var section = $(this).data('nav-section'),
-				navbar = $('#navbar');
-
+			navbar = $('#navbar');
 			if ($('[data-section="' + section + '"]').length) {
 				$('html, body').animate({
 					scrollTop: $('[data-section="' + section + '"]').offset().top - 55
 				}, 500);
 			}
-
 			if (navbar.is(':visible')) {
 				navbar.removeClass('in');
 				navbar.attr('aria-expanded', 'false');
 				$('.js-colorlib-nav-toggle').removeClass('active');
 			}
-
 			event.preventDefault();
 			return false;
 		});
-
-
 	};
 
 	// Reflect scrolling in navigation
 	var navActive = function (section) {
-
 		var $el = $('#navbar > ul');
 		$el.find('li').removeClass('active');
 		$el.each(function () {
 			$(this).find('a[data-nav-section="' + section + '"]').closest('li').addClass('active');
 		});
-
 	};
-
-	var navigationSection = function () {
-
-		var $section = $('section[data-section]');
-
-		$section.waypoint(function (direction) {
-
-			if (direction === 'down') {
-				navActive($(this.element).data('section'));
-			}
-		}, {
-			offset: '150px'
-		});
-
-		$section.waypoint(function (direction) {
-			if (direction === 'up') {
-				navActive($(this.element).data('section'));
-			}
-		}, {
-			offset: function () { return -$(this.element).height() + 155; }
-		});
-
-	};
-
-
-
-
-
 
 	var sliderMain = function () {
-
 		$('#colorlib-hero .flexslider').flexslider({
 			animation: "fade",
 			slideshowSpeed: 5000,
@@ -230,145 +111,36 @@
 					$('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
 				}, 500);
 			}
-
 		});
-
-	};
-
-	// var stickyFunction = function () {
-
-	// 	var h = $('.image-content').outerHeight();
-
-	// 	if ($(window).width() <= 992) {
-	// 		$("#sticky_item").trigger("sticky_kit:detach");
-	// 	} else {
-	// 		$('.sticky-parent').removeClass('stick-detach');
-	// 		$("#sticky_item").trigger("sticky_kit:detach");
-	// 		$("#sticky_item").trigger("sticky_kit:unstick");
-	// 	}
-
-	// 	$(window).resize(function () {
-	// 		var h = $('.image-content').outerHeight();
-	// 		$('.sticky-parent').css('height', h);
-
-
-	// 		if ($(window).width() <= 992) {
-	// 			$("#sticky_item").trigger("sticky_kit:detach");
-	// 		} else {
-	// 			$('.sticky-parent').removeClass('stick-detach');
-	// 			$("#sticky_item").trigger("sticky_kit:detach");
-	// 			$("#sticky_item").trigger("sticky_kit:unstick");
-
-	// 			$("#sticky_item").stick_in_parent();
-	// 		}
-
-
-
-
-	// 	});
-
-	// 	$('.sticky-parent').css('height', h);
-
-	// 	// $("#sticky_item").stick_in_parent();
-
-	// };
-
-	var owlCrouselFeatureSlide = function () {
-		$('.owl-carousel').owlCarousel({
-			animateOut: 'fadeOut',
-			animateIn: 'fadeIn',
-			autoplay: true,
-			loop: true,
-			margin: 0,
-			nav: true,
-			dots: false,
-			autoHeight: true,
-			items: 1,
-			navText: [
-				"<i class='icon-arrow-left3 owl-direction'></i>",
-				"<i class='icon-arrow-right3 owl-direction'></i>"
-			]
-		})
 	};
 
 	var setGallery = function () {
-
-		//const container = document.querySelector('.grid');
 		var elements = document.getElementsByClassName("grid");
-		console.log(elements);
-
 		for (var index = 0; index < elements.length; index++) {
 			const gridify = new Gridify();
 
 			var container = elements[index];
-			
 			container.addEventListener('gridify:init', (ev) => {
 				console.log('Init handler');
 			}, false);
-
 			container.addEventListener('gridify:resized', (ev) => {
 				console.log('Resized handler');
 			}, false);
 		};
-
 	};
 
-	 var magnifPopup = function() {
-		$('.popup-img').magnificPopup({
-	 	  type: 'image',
-	 	  removalDelay: 300,
-	 	  mainClass: 'mfp-with-zoom',
-	 	  gallery: {
-	 		enabled: true
-	 	  },
-	 	  zoom: {
-	 		enabled: true, // By default it's false, so don't forget to enable it
-	
-	 		duration: 300, // duration of the effect, in milliseconds
-	 		easing: 'ease-in-out', // CSS transition easing function
-	
-	 		// The "opener" function should return the element from which popup will be zoomed in
-			// and to which popup will be scaled down
-			// By defailt it looks for an image tag:
-		opener: function(openerElement) {
-			  // openerElement is the element on which popup was initialized, in this case its <a> tag
-			  // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-	 		  return openerElement.is('img') ? openerElement : openerElement.find('img');
-	 		}
-	 	  }
-	 	});
-	   };
-	
 	// Document on load.
 	$(function () {
 		fullHeight();
-		counter();
-		counterWayPoint();
-		contentWayPoint();
 		burgerMenu();
-
 		clickMenu();
-		// navActive();
-		navigationSection();
-		// windowScroll();
-
-
 		mobileMenuOutsideClick();
 		sliderMain();
-		// stickyFunction();
-		owlCrouselFeatureSlide();
 		setGallery();
-		// magnifPopup();
-
 	});
-
-
 }());
 
-$(document).ready(function() {
-
-
-  
+$(document).ready(function () {
 	$('.zoom-gallery').magnificPopup({
 		delegate: 'a',
 		type: 'image',
@@ -377,7 +149,7 @@ $(document).ready(function() {
 		mainClass: 'mfp-with-zoom mfp-img-mobile',
 		image: {
 			verticalFit: true,
-			titleSrc: function(item) {
+			titleSrc: function (item) {
 				return item.el.attr('title');
 			}
 		},
@@ -387,10 +159,9 @@ $(document).ready(function() {
 		zoom: {
 			enabled: true,
 			duration: 300, // don't foget to change the duration also in CSS
-			opener: function(element) {
+			opener: function (element) {
 				return element.find('img');
 			}
 		}
-		
 	});
 });
